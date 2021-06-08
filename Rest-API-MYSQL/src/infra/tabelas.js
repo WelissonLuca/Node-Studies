071f3879ad414a62f05d84a06fb8ce1c673058cf
Welisson luca <welissonluca17@gmail.com>
2021-06-08 00:43:55 -0300

    feat: project API - express and mysql > add moment tratament dates format insert sql

--- Rest-API-MYSQL/src/infra/tabelas.js
+++ Rest-API-MYSQL/src/infra/tabelas.js
@@ -1,13 +1,13 @@
 class Tabelas {
 	init(conexao) {
-    this.conexao = conexao;
-    
-    this.criarAtendimentos()
+		this.conexao = conexao;
+
+		this.criarAtendimentos();
 	}
 
 	criarAtendimentos() {
 		const sql =
-			"CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id))";
+			"CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL,data datetime NOT NULL, dataCriacao datetime NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id))";
 		this.conexao.query(sql, err => {
 			if (err) console.error(err);
 			else console.log("Tabela atendimento criada com sucesso");
