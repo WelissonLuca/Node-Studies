988771a1cfac423f78db83594c4dcf181aebb170
Welisson luca <welissonluca17@gmail.com>
2021-06-08 00:22:56 -0300

    feat: project API - express and mysql > add table atendimento in db

--- Rest-API-MYSQL/src/index.js
+++ Rest-API-MYSQL/src/index.js
@@ -1,10 +1,11 @@
 const customExpress = require("./config/customExpress");
 const conexao = require("./infra/conexao");
-
+const Tabelas = require("./infra/tabelas")
 conexao.connect(err => {
 	if (err) console.error(err);
 	else {
 		console.log("connectado com sucesso");
+		Tabelas.init(conexao);
 		const app = customExpress();
 		app.listen(3000, () => console.log("server listening"));
 	}
---
+++ Rest-API-MYSQL/src/infra/tabelas.js
@@ -0,0 +1,18 @@
+class Tabelas {
+	init(conexao) {
+    this.conexao = conexao;
+    
+    this.criarAtendimentos()
+	}
+
+	criarAtendimentos() {
+		const sql =
+			"CREATE TABLE Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id))";
+		this.conexao.query(sql, err => {
+			if (err) console.error(err);
+			else console.log("Tabela atendimento criada com sucesso");
+		});
+	}
+}
+
+module.exports = new Tabelas();
