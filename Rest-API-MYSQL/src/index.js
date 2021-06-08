fea79f6f7f4b89d2831ba1fe903c2e26d9e2419c
Welisson luca <welissonluca17@gmail.com>
2021-06-07 23:23:02 -0300

    feat: project API - express and mysql > add nodemon

--- Rest-API-MYSQL/src/index.js
+++ Rest-API-MYSQL/src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const app = express();
 
+app.listen(3000, () => console.log("server listening"));
 
 
-app.listen(3000, () => console.log('server listening'))
+app.get('/', (req, res) => res.send('ola'))
